9d7f08bcb41d5938db47d74e05c7af3d08ad6a2d
shashikant jadhav <jadhavshashikant677@gmail.com>
2025-06-18 02:08:09 +0530

    fifth commit

--- down.py
+++ down.py
@@ -56,6 +56,4 @@ def check():
     return "✅ Flask API is running"
 
 if __name__ == '__main__':
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port, debug=True)
+    app.run(debug=True)
